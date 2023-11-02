b47d211a5a82fbead307bd236bd79c9882f9e25f
Memo Eguiluz <memo.eguiluz@gmail.com>
2023-11-02 16:04:39 -0600

    First packages follow-along

---
+++ modules/convert_temp.py
@@ -0,0 +1,12 @@
+# This file will be used as a module on a different file
+
+def c_to_f(c):
+    """Convert celcius to fahrenheit and return result."""
+    f = (c * 1.8) +32
+
+    return f
+
+def f_to_c(f):
+    """Convert fahrenheit to celcius"""
+    c = (f - 32) / 1.8
+    return c
---
+++ modules/temperature.py
@@ -0,0 +1,7 @@
+from convert_temp import *
+
+fahrenheit = c_to_f(100)
+print(f"F = {fahrenheit}")
+
+celcius = f_to_c(212)
+print(f"C = {celcius}")
