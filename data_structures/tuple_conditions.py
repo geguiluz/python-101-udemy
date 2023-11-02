79bb4f20bb00865a3ef4569416a76673721c79dd
geguiluz <memo.eguiluz@gmail.com>
2023-11-02 10:52:24 -0600

    More tuple examples

---
+++ data_structures/tuple_conditions.py
@@ -0,0 +1,7 @@
+numbers = (1, 8, 4, 4)
+
+if 8 in numbers:
+    print(f"8 is in tuple")
+
+if 2 not in numbers:
+    print(f"2 is not in tuple")
---
+++ data_structures/tuple_single_type.py
@@ -0,0 +1,7 @@
+thing = ("Abacus",)
+
+print(thing)
+
+# Type of thing: <class 'tuple'>
+type_of_thing = type(thing)
+print(f"Type of thing: {type_of_thing}")
