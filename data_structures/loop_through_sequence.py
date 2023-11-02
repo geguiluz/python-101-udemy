0a6bb388de1c92f36b14a598b01fed80ad77da9e
Memo Eguiluz <memo.eguiluz@gmail.com>
2023-11-02 11:16:06 -0600

    First approach to looping through dictionaries

--- data_structures/loop_through_sequence.py
+++ data_structures/loop_through_sequence.py
@@ -4,3 +4,12 @@ for fruit in fruit_list:
     print(fruit)
 
 # We're going to loop through dictionary values
+student_count = {
+        "course1": 923,
+        "course2": 7738,
+        "course3": 112,
+    }
+for k, v in student_count.items():
+    print(f"{k}: {v}")
+
+
