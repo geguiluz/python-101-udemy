4a818d53a5a36ff51abe2eb48b3398ff44e6df23
geguiluz <memo.eguiluz@gmail.com>
2023-11-02 11:32:38 -0600

    Added additional looping exercises

--- data_structures/loop_through_sequence.py
+++ data_structures/loop_through_sequence.py
@@ -3,13 +3,24 @@ fruit_list = ["Apple", "Banana", "Orange"]
 for fruit in fruit_list:
     print(fruit)
 
-# We're going to loop through dictionary values
 student_count = {
-        "course1": 923,
-        "course2": 7738,
-        "course3": 112,
-    }
+    "course1": 923,
+    "course2": 7738,
+    "course3": 112,
+}
+
+# We're going to loop through dictionary values
 for k, v in student_count.items():
     print(f"{k}: {v}")
 
+# Count the total number of students
+total_students = 0
+for k, v in student_count.items():
+    total_students += v
 
+print(f"Total students: {total_students}")
+
+# Get all keys from dictionary
+# dict_keys(['course1', 'course2', 'course3'])
+courses = student_count.keys()
+print(courses)
