9e3c32712310d8c0b3f4f75266a4b2b1e2a23e85
Memo Eguiluz <memo.eguiluz@gmail.com>
2023-11-03 00:14:21 -0600

    Basic oop examples

--- oop/car.py
+++ oop/car.py
@@ -2,6 +2,13 @@ class Car:
     """Represents a car object"""
     miles = 0
     
+    def __init__(self, color, make, model, miles=0):
+        """Set initial details of car."""
+        self.color = color
+        self.make = make
+        self.model = model
+        self.miles = miles
+
     # Methods must receive self as first parameter
     def add_miles(self, miles):
         """Increase miles by given number."""
@@ -13,7 +20,7 @@ class Car:
                                                       
 
 # We create an instance of Car
-astra = Car()
+astra = Car("Red", "Vauxhall", "Astra")
 print(astra.miles)
 
 # We can increase the variable within the object, but it's not deal
@@ -24,3 +31,8 @@ astra.add_miles(100)
 print(astra.miles)
 
 astra.display_miles()
+
+prius = Car("Blue", "Toyota", "Prius", 1000)
+prius.display_miles()
+prius.add_miles(50)
+prius.display_miles()
