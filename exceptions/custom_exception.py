e3104aaf2904d3beb0709d89e6ec869fdf124ee5
Memo Eguiluz <memo.eguiluz@gmail.com>
2023-11-11 10:10:14 -0600

    Exception raising

---
+++ exceptions/custom_exception.py
@@ -0,0 +1,20 @@
+class ValidationError(Exception):
+    """Exception raised for validation errors"""
+    def __init__(self, field, msg):
+        """Set error details"""
+        self.field = field
+        self.msg = msg
+
+def register(username, password):
+    """Register a new user in the system"""
+    if not username: 
+        raise ValidationError('username', 'Field is required')
+    if not password:
+        raise ValidationError('password', 'All fields are required')
+    print("Registered new user")
+
+
+try:
+    register('','')
+except ValidationError as ve:
+    print(f"Error validating {ve.field}: {ve.msg}")
