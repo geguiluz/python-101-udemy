5806674a62021d8fe7bb2482e5ed49aa7087849d
geguiluz <memo.eguiluz@gmail.com>
2023-11-21 23:57:53 -0600

    Created file handling moudle example and implementation

---
+++ modules/files/file_handler.py
@@ -0,0 +1,48 @@
+import os
+
+
+def _set_dir_path(sub_dir: str):
+    """
+    Outputs the full path under current working directory with the
+    specified sub-directory name.
+    """
+    # Get current working directory
+    base_dir = os.getcwd()
+    sub_dir = "output"
+    full_dir_path = os.path.join(base_dir, sub_dir)
+    print(full_dir_path)
+    return full_dir_path
+
+
+def write_file(
+    file_name: str,
+    sub_dir: str,
+    contents: any,
+    mode: str = "a",
+    end_with_return: bool = "True",
+):
+    """
+    Writes to a file with a given file_name inside sub_dir under cwd.
+
+    Depending on the mode we use, we will get the following results
+    w: Overrides existing content
+    a: Appends to existing contents (same line)
+
+    If we want to add content in the sa,e line, we must add argument
+    end_with_return="False" otherwise new line defaults to True.
+    """
+    full_dir_path = _set_dir_path(sub_dir)
+    os.makedirs(full_dir_path, exist_ok=True)
+
+    # function os.path.join() creates the full path with our filename
+    file_path = os.path.join(full_dir_path, file_name)
+
+    # Set line end
+    if end_with_return:
+        line_end = "\n"
+    else:
+        line_end = ""
+
+    # Create file
+    with open(file_path, mode) as msg_file:
+        msg_file.write(contents + line_end)
---
+++ modules/handling_files.py
@@ -0,0 +1,5 @@
+"""Handling files using imports and custom module for file handling"""
+
+from files import file_handler as fh
+
+fh.write_file("test_file.txt", "output", "Other contents", "w")
