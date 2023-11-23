00d326fdd699c68dd85d3b1138b28d648d5b04d2
geguiluz <memo.eguiluz@gmail.com>
2023-11-23 01:13:06 -0600

    Multiple experiments using pandas and json files

--- modules/read_json_to_pandas.py
+++ modules/read_json_to_pandas.py
@@ -1,11 +1,25 @@
-# import pandas as pd
-import json
+import pandas as pd
 import files.file_handler as fh
 
 # Reading JSON data from a file
-f = fh.open_file("data.json", "input")
-json_data = json.load(f)
-print(json_data)
+json_data = fh.open_json_file("data.json", "input")
 
+print(json_data)
 # Converting JSON data to a pandas DataFrame
-# df = pd.read_json(json_data)
+"""
+# Deprecated pd.read_json(json_data) Literal json strings are no longer valid.
+# Provide full path instead:
+df = pd.read_json(
+    "/Users/memo/code-projects/self-learning/learning-python/python-101-udemy/input/data.json"
+)
+"""
+"""
+# Didn't quite like the following result as json is not normalized
+df = pd.json_normalize(json_data["orderLogisticalInformation"])
+print(df)
+"""
+# Other option that might be useful:
+df = pd.json_normalize(json_data)
+df.to_csv(
+    "/Users/memo/code-projects/self-learning/learning-python/python-101-udemy/output/data_pandas.csv"
+)
